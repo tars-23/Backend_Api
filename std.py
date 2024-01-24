2e6a61ee82c56ba44eb53ea4272ea8585a50d7ad
tars-23 <tars223@hotmail.com>
2024-01-25 00:29:36 +0700

    Update_GreetAndGet

---
+++ std.py
@@ -0,0 +1,20 @@
+from pymongo.mongo_client import MongoClient
+from flask import request,Flask,jsonify
+from flask_basicauth import BasicAuth
+
+uri = "mongodb+srv://auto123:auto123@cluster0.amvg1ng.mongodb.net/?retryWrites=true&w=majority"
+
+app = Flask(__name__) 
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+
+client.admin.command('ping')
+db = client["students"]
+collection = db["std_info"]
+
+@app.route("/")
+def Greet():
+    return "<p>Welcome to Student Management API</p>"
+
